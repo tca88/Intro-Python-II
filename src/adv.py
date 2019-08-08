db91fd85eb1d21207192cb0c6a7e00a6d171b6eb
tca88 <trishnaamin@gmail.com>
2019-08-08 18:12:15 -0400

    Add item class with attributes for name and description in item.py. Import Item class into adv.py. In adv.py, add array of items to each room. In Item.py, create on_take and on_drop methods for player to be able to add or remove from their inventory.

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,6 @@
 from room import Room
 from player import Player
+from item import Item
 import textwrap
 import sys
 
@@ -7,8 +8,7 @@ import sys
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
@@ -37,6 +37,16 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
+room['outside'].items = [Item('Crystal-Skull', 'A really cool crystal skull'), Item(
+    'A-Rock', 'A cool rock')]
+room['foyer'].items = [Item('Cloak-of-Invisibility', 'A cloak that makes people invisible')]
+room['overlook'].items = [Item('Vision-Stone', 'A stone that can show you the future'), Item(
+    'Infinity-Stone', 'You can live forever with it'), Item('Death-Stone', 'You can kill people with it')]
+room['narrow'].items = [Item('A-Vase', 'Does nothing, it is a vase'), Item(
+    'A-Feather', 'Just a plain old feather')]
+room['treasure'].items = [Item('Gold', 'So much goooold'), Item(
+    'Diamond', 'So you can shine bright...like a diamond')]
+
 #
 # Main
 #
@@ -54,39 +64,84 @@ room['treasure'].s_to = room['narrow']
 # If the user enters "q", quit the game.
 playerName = input("Please enter your name and let the adventure begin!: ")
 p = Player(playerName, room['outside'])
-print(f"Welcome, {p.name}! Let's begin the adventure!")
-
+print(f"Welcome, {p.name}! You're currently in {p.current_room.name}.\n Here's what awaits you: {p.current_room.description}.\n Now let's begin the adventure!")
+userInput = ''
 inRoom = True
-while inRoom:
-    try:
-        print(f"You're currently in {p.current_room.name}! Here is what awaits you: {p.current_room.description}")
-        userInput = input('Pick a direction to move to next.\n Enter n for North, s for South, e for East or w for West. To quit the game, enter q. ')
-        cardinalDirection = userInput
-        if cardinalDirection == "q":
-            inRoom = False
-            print("You've successfully exited the game")
-            continue
+while inRoom and not userInput == 'q':
+    current_room_items = p.current_room.items
+    print(f'You are now in {p.current_room.name}!\n')
+    print(f'Here is what awaits you next. Choose your next direction accordingly: {p.current_room.description}. ')
+    print('Here are the items in the room you are in: ')
+    for item in current_room_items:
+        print(item.name)
+    userInput = input('Enter n, e, w, s to move to a new room.\nPick up items by using get `item name`\nDrop an item with drop `item name`.\n You can also enter i to see your current inventory.')
+    actionInputs = userInput.split(' ')
 
-        if cardinalDirection == 'n' or cardinalDirection == 's' or cardinalDirection == 'e' or cardinalDirection == 'w':
+    if len(actionInputs) == 2:
+        if actionInputs[0] == 'get':
+            roomItems = p.current_room.items
+            for item in roomItems:
+                if item.name == actionInputs[1]:
+                    p.current_room.items.remove(item)
+                    p.items.append(item)
+                    item.on_take()
+                    continue
+                else:
+                    print('The item is not in this room.')
+
+        if actionInputs[0] == 'drop':
+            for item in p.items:
+                if item.name == actionInputs[1]:
+                    p.items.remove(item)
+                    p.current_room.items.append(item)
+                    item.on_drop()
+                    continue
+                else:
+                    print('You can\'t drop something that doesn\'t exist')
+    else:
+        if userInput == 'n' or userInput == 's' or userInput == 'e' or userInput == 'w':
             try:
-                if cardinalDirection == 'n':
+                if userInput == 'n':
                     newRoom = p.current_room.n_to
-                if cardinalDirection == 's':
+                if userInput == 's':
                     newRoom = p.current_room.s_to
-                if cardinalDirection == 'e':
+                if userInput == 'e':
                     newRoom = p.current_room.e_to
-                if cardinalDirection == 'w':
+                if userInput == 'w':
                     newRoom = p.current_room.w_to
                 p.current_room = newRoom
-                print(f"{p.current_room.name}")
                 continue
+            except:
+                print(f'Uh oh, looks like {userInput} is not a valid direction.\nYou will need to restart the game.\n')
+                sys.exit(1)
+                continue
+        
+        elif userInput == 'i':
+            print('This is your current inventory:')
+            for item in p.items:
+                print(item.name)
+        
+        elif actionInputs[0] == 'get':
+            roomItems = p.current_room.items
+            for item in roomItems:
+                if item.name == actionInputs[1]:
+                    p.current_room.items.remove(item)
+                    p.items.append(item)
+                    item.on_take()
+                    continue
+                else:
+                    print('The item is not in this room.')
 
-            except ValueError:
-                print("That's not a valid direction. Please try again.")
-                continue
-    except:
-        print(f'Uh oh, looks like {cardinalDirection} is not a valid direction.\nYou will need to restart the game.\n')
-        sys.exit(1)
-        continue
+        elif actionInputs[0] == 'drop':
+            for item in p.items:
+                if item.name == actionInputs[1]:
+                    p.items.remove(item)
+                    p.current_room.items.append(item)
+                    item.on_drop()
+                    continue
+                else:
+                    print('You can\'t drop something that doesn\'t exist')
+        else:
+            print(f'{userInput} is an invalid move.\nPlease enter n, e, s, or w\n')
     
 print('The game has ended!')
--- src/room.py
+++ src/room.py
@@ -1,5 +1,6 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
 class Room:
     def __init__(self, name, description):
         self.name = name
@@ -8,3 +9,4 @@ class Room:
         self.e_to = None
         self.w_to = None
         self.s_to = None
+        self.items = []
